7a18b5ad30acbb9344dc037acf2eb61e8a460c59
Siriphan Boonmee <m.basicman@msn.com>
2017-06-01 16:27:34 +0700

    update

--- index.php
+++ index.php
@@ -31,14 +31,29 @@
         }
     }*/
 
-    $ch = curl_init('https://gw1.judopay-sandbox.com');
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,
-        "apiToken=jwmXGbpb87xvDM4B&apiSecret=601dc0a93d2752f5041bdb9a53dc1bf0b4e8ef0f1b03f737416fcf3be1a20b7d&judoId=100826-205&useProduction=false");
+    $url = 'https://gw1.judopay-sandbox.com/transactions/payments';
+    $fields = array(
+        'yourConsumerReference' => '5678849',
+        'yourPaymentReference' => '9908865755',
+        'cardNumber' => '4976000000003436',
+        'expiryDate' => '12/20',
+        'cv2' => '452',
+        'amount' => 1.01,
+        'currency' => 'GBP',
+        'judoId' => '100826-205'
+    );
+
+    //url-ify the data for the POST
+    foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+    rtrim($fields_string, '&');
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, count($fields));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'apiVersion: 5.2.0',
-            'userAgent: Judopay PHP v7.0 SDK v2.1.0',
-            'Content-Type: application/json')
+            'API-Version: 5.2.0',
+            'Authorization: Basic {authstring}',
+            'Accept: application/json')
     );
 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
