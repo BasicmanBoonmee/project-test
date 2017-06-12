49bf008ab0febf6e84af2342dbe1545452c96c5e
Siriphan Boonmee <m.basicman@msn.com>
2017-06-12 10:49:01 +0700

    update

--- index-calendar.php
+++ index-calendar.php
@@ -93,7 +93,7 @@
     } else {
         echo "Upcoming list calendar :<br />";
         foreach ($listCalendar->getItems() as $calendar) {
-            echo print_r($calendar,true)."<br />";
+            echo $calendar->getSummary()."<br />";
         }
     }
 
